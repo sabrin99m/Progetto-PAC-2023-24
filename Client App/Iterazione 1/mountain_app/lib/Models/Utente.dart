fd5ff5f04b0662f8e13b2bc9e3975c2677552875
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2023-12-31 18:15:47 +0100

    Modifiche minori ad Utente

--- Client App/Iterazione 1/mountain_app/lib/Models/Utente.dart
+++ Client App/Iterazione 1/mountain_app/lib/Models/Utente.dart
@@ -1,25 +1,24 @@
 class Utente {
-  final String nome;
-  final String cognome;
-  final Uri urlImmagineProfilo;
+  String nome;
+  String cognome;
+  bool isLoggedIn;
+  Uri urlImmagineProfilo;
 
-  Utente({
-    required this.nome,
-    required this.cognome,
-    required this.urlImmagineProfilo,
-  });
+  Utente(this.nome, this.cognome, this.isLoggedIn, this.urlImmagineProfilo) {}
 
 //Dati mock usati per lo sviluppo
   static Utente utenteMock1 = Utente(
-      nome: "Cristian",
-      cognome: "Tironi",
-      urlImmagineProfilo: Uri(
+      "Cristian",
+      "Tironi",
+      true,
+      Uri(
           path:
               "https://images.pexels.com/photos/19551874/pexels-photo-19551874/free-photo-of-golden-retriever-in-christmas-headband.jpeg"));
   static Utente utenteMock2 = Utente(
-      nome: "Federico",
-      cognome: "Imberti",
-      urlImmagineProfilo: Uri(
+      "Federico",
+      "Imberti",
+      true,
+      Uri(
           path:
               "https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg"));
 
