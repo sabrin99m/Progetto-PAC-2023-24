50e7e421f2471da0f172405326e1585464be393d
Cristian <tironi18@gmail.com>
2024-01-09 18:22:40 +0100

    SecurityConfig

--- Backend/project/src/main/java/com/pac/gestoreeventi/Main.java
+++ Backend/project/src/main/java/com/pac/gestoreeventi/Main.java
@@ -3,7 +3,12 @@ package com.pac.gestoreeventi;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class Main {
---
+++ Backend/project/src/main/java/com/pac/gestoreeventi/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.pac.gestoreeventi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+     //@Autowired
+     //private UserService userService;
+
+     //@Override
+     //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+     //    auth.userDetailsService(userService);
+     //}
+
+     @Override
+     protected void configure(HttpSecurity http) throws Exception {
+         http.authorizeRequests()
+                 .anyRequest().authenticated().and().csrf().disable().sessionManagement()
+                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().httpBasic();
+     }
+
+     @Bean
+     public PasswordEncoder getPasswordEncoder() {
+         return NoOpPasswordEncoder.getInstance();
+     }
+}
