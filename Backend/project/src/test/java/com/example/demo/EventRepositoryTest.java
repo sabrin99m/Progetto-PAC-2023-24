f56dc59b71c2f2c1fe3c1a09924e3356577456fe
sabrin99m <93140098+sabrin99m@users.noreply.github.com>
2024-01-18 12:58:07 +0100

    Add files via upload

---
+++ Backend/project/src/test/java/com/example/demo/EventRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.pac.gestoreeventi.eventsManagement;
+
+
+import java.sql.Time;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class EventRepositoryTest {
+
+	@Autowired
+	private EventRepository underTest;
+	
+	@Test
+	void findById() {
+			
+		String stringaTest = "test";
+		//given
+		@SuppressWarnings("deprecation")
+		Event expected = new Event(1, stringaTest, stringaTest, EventLevel.MEDIUM, new Date(2024, 01, 19), stringaTest, stringaTest, stringaTest, stringaTest, stringaTest, stringaTest, new Time(15, 30, 00), 4);
+		
+		underTest.save(expected);
+		
+		//when
+		Event result = underTest.findById(1).get();
+		
+		//then
+		assertThat(expected).isEqualTo(result);
+	}
+}
