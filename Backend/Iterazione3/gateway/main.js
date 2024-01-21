1eab52d4d99e35e14a8265901732f32d5fa04d5b
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2024-01-21 19:49:33 +0100

    Aggiunti vari eventi di Get e Delete

--- Backend/Iterazione3/gateway/main.js
+++ Backend/Iterazione3/gateway/main.js
@@ -31,6 +31,126 @@ app.get('/events', async (req, res) => {
 
 });
 
+app.get('/events/:id', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+
+    //Se non viene fornita autorizzazione
+    if (!authorization) {
+      return res.sendStatus(401)
+    }
+
+    //Richiesta get al server
+    const getResponse = await axios.get(`http://64.23.165.209:8080/events/${req.params.id}`, {
+      headers: {
+        'Authorization': authorization
+      }
+    });
+
+    buildResponse(getResponse, res)
+
+  } catch (error) {
+    handleError(error, res)
+  }
+
+});
+
+app.get('/profiles', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+
+    //Se non viene fornita autorizzazione
+    if (!authorization) {
+      return res.sendStatus(401)
+    }
+
+    //Richiesta get al server
+    const getResponse = await axios.get('http://64.23.165.209:8080/profiles', {
+      headers: {
+        'Authorization': authorization
+      }
+    });
+
+    buildResponse(getResponse, res)
+
+  } catch (error) {
+    handleError(error, res)
+  }
+
+});
+
+app.get('/profiles/:id', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+
+    //Se non viene fornita autorizzazione
+    if (!authorization) {
+      return res.sendStatus(401)
+    }
+
+    //Richiesta get al server
+    const getResponse = await axios.get(`http://64.23.165.209:8080/profiles/${req.params.id}`, {
+      headers: {
+        'Authorization': authorization
+      }
+    });
+
+    buildResponse(getResponse, res)
+
+  } catch (error) {
+    handleError(error, res)
+  }
+
+});
+
+app.delete('/events/:id', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+
+    //Se non viene fornita autorizzazione
+    if (!authorization) {
+      return res.sendStatus(401)
+    }
+
+    //Richiesta delete al server
+    const getResponse = await axios.delete(`http://64.23.165.209:8080/events/${req.params.id}`, {
+      headers: {
+        'Authorization': authorization
+      }
+    });
+
+    buildResponse(getResponse, res)
+
+  } catch (error) {
+    handleError(error, res)
+  }
+
+});
+
+app.delete('/profiles/:id', async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+
+    //Se non viene fornita autorizzazione
+    if (!authorization) {
+      return res.sendStatus(401)
+    }
+
+    //Richiesta delete al server
+    const getResponse = await axios.delete(`http://64.23.165.209:8080/profiles/${req.params.id}`, {
+      headers: {
+        'Authorization': authorization
+      }
+    });
+
+    buildResponse(getResponse, res)
+
+  } catch (error) {
+    handleError(error, res)
+  }
+
+});
+
 function handleError(error, res) {
   console.error(error);
   res.status(500).send('Internal Server Error');
@@ -42,6 +162,10 @@ function buildResponse(response, res) {
       //Credenziali fornite sono sbagliate
       res.sendStatus(401)
       break;
+
+    case 404:
+      //Risorsa non trovata
+      res.sendStatus(404)
 
     case 200:
       //Successo
