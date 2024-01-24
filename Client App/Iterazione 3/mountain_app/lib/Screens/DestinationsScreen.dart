adc36576461c0096d2051d4d2dc044f9bd280ce3
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2024-01-24 10:35:49 +0100

    Aggiunte escursioni scaricate alla pagina principale

--- Client App/Iterazione 3/mountain_app/lib/Screens/DestinationsScreen.dart
+++ Client App/Iterazione 3/mountain_app/lib/Screens/DestinationsScreen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mountain_app/Managers/EventsManager.dart';
 import 'package:mountain_app/Models/Escursione.dart';
 import 'package:mountain_app/Models/Utente.dart';
 import 'package:mountain_app/Views/EventDetailsView.dart';
 import 'package:mountain_app/Views/TileView.dart';
 
-class DestinationsScreen extends StatelessWidget {
+class DestinationsScreen extends StatefulWidget {
   final List<Escursione> escursioni;
   final Utente utente;
   const DestinationsScreen({
@@ -14,21 +15,32 @@ class DestinationsScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<DestinationsScreen> createState() => _DestinationsScreenState();
+}
+
+class _DestinationsScreenState extends State<DestinationsScreen> {
+  @override
+  void initState() {
+    EventsManger().fetchEvents();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: escursioni.length,
+        itemCount: EventsManger().escursioni.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: TileView(escursione: escursioni[index]),
+            title: TileView(escursione: EventsManger().escursioni[index]),
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => EventDetailsView(
-                    escursione: escursioni[index],
+                    escursione: EventsManger().escursioni[index],
                     listaEscursioni: Utente.utenteMock1.iscrizioni,
-                    utente: utente,
+                    utente: widget.utente,
                   ),
                 ),
               );
