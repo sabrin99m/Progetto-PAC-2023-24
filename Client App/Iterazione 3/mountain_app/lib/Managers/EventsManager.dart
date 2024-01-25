ca8c7b8fb29f231e515adf07c1dda6c44d7cf741
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2024-01-25 10:57:19 +0100

    Refactoring

--- Client App/Iterazione 3/mountain_app/lib/Managers/EventsManager.dart
+++ Client App/Iterazione 3/mountain_app/lib/Managers/EventsManager.dart
@@ -1,36 +1,26 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:mountain_app/Models/Escursione.dart';
+import 'package:mountain_app/Utilities/ConstantValues.dart';
 import 'package:http/http.dart' as http;
 
 /// Manager responsabile delle chiamate http al server. Creato con un pattern Singleton
 class EventsManger {
-  ///Indica se la pagina si sta caricando
-  late bool isLoading;
-
   //Istanza statica privata della classe
   static final EventsManger _instance = EventsManger._internal();
-
-  late String _baseIpGateway;
-  late String _baseIpLocal;
 
   factory EventsManger() {
     return _instance;
   }
 
-  EventsManger._internal() {
-    _baseIpGateway = "http://165.227.152.216:8085";
-    _baseIpLocal = "http://localhost:8085";
-    isLoading = false;
-  }
+  EventsManger._internal() {}
 
   Future<List<Escursione>> fetchEvents(String username, String password) async {
     String basicAuth =
         'Basic ${base64Encode(utf8.encode('$username:$password'))}';
 
-    isLoading = true;
     try {
-      final response = await http.get(Uri.parse('$_baseIpGateway/events/'),
+      final response = await http.get(Uri.parse('${baseIpGateway}/events/'),
           headers: {HttpHeaders.authorizationHeader: basicAuth});
 
       List<dynamic> decoded = json.decode(response.body);
@@ -42,7 +32,6 @@ class EventsManger {
       print(e);
     }
 
-    isLoading = false;
     return [];
   }
 
@@ -52,7 +41,7 @@ class EventsManger {
     String basicAuth =
         'Basic ${base64Encode(utf8.encode('$username:$password'))}';
 
-    final response = await http.get(Uri.parse('$_baseIpGateway/events/$id'),
+    final response = await http.get(Uri.parse('$baseIpGateway/events/$id'),
         headers: {HttpHeaders.authorizationHeader: basicAuth});
 
     if (response.statusCode == 200) {
---
+++ Client App/Iterazione 3/mountain_app/lib/Utilities/ConstantValues.dart
@@ -0,0 +1,2 @@
+String baseIpGateway = "http://165.227.152.216:8085";
+String baseIpLocal = "http://localhost:8085";
