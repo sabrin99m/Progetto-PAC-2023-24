9cf6ffb59fad37101f2d06d881c981291115e92c
sabrin99m <93140098+sabrin99m@users.noreply.github.com>
2024-02-03 18:14:49 +0100

    testing

---
+++ Backend/Iterazione3/project/src/test/java/com/pac/webserver/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pac.gestoreeventi;
+
+import com.pac.gestoreeventi.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Main.class)
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
