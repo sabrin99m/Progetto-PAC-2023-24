1f2252692291facccaf23347643869035b6fba1b
sabrin99m <93140098+sabrin99m@users.noreply.github.com>
2024-02-04 15:55:41 +0100

    unit test

---
+++ Backend/Iterazione3/project/src/test/java/com/pac/gestoreeventi/ReservationRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.pac.gestoreeventi;
+
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.pac.gestoreeventi.reservationManagement.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ReservationRepositoryTest {
+
+	@Autowired
+	private ReservationRepository underTest;
+	
+	@Test
+	void findById() {
+			
+		//given
+		Date date = new Date(100);
+		Reservation expected = new Reservation(new Integer(1), date, new Boolean(true));
+		underTest.save(expected);
+		
+		//when
+		Reservation result = underTest.findById(expected.getId()).get();
+		
+		//then
+		assertThat(expected.getId()).isEqualTo(result.getId());
+	}
+}
